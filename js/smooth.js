6aae1d60d1fd87990ca69c1725f112f73c945e3d
carlos <fantaso.code@gmail.com>
2018-02-28 12:12:02 +0100

    nav bar text
    hover text color stays white the same

--- js/smooth.js
+++ js/smooth.js
@@ -10,3 +10,20 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#f0f0f0');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
+    }
+});
