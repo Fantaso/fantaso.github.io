275e66a732f55e08ba91563b607e575d05a03ef7
carlos <fantaso.code@gmail.com>
2018-02-28 19:17:53 +0100

    Nav Bar & Icons
    shadows:
    -arrow button to go to top
    -icons in "about me" section
    -icons in "banner" section
    -logo in "nav-bar"
    Navageation bar:
    -changed the background color

--- js/nav-color.js
+++ js/nav-color.js
@@ -3,7 +3,7 @@ var a = $("#fh5co-about").offset().top;
 $(document).scroll(function(){
     if($(this).scrollTop() > a)
     {   
-       $('#nav-bar').css({"background":"rgba(0,0,0, 0.3)"});
+       $('#nav-bar').css({"background":"rgba(0,174,213, 0.7)"});
     } else {
        $('#nav-bar').css({"background":"rgba(0,0,0, 0.1)"});
     }
