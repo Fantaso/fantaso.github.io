fb2cd8223e3dbf1a1c4b400b49262d29b84aea32
carlos <fantaso.code@gmail.com>
2018-03-03 01:32:24 +0100

    div to section

--- nav-bar-autohover.js
+++ nav-bar-autohover.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-   $("div").mouseenter(function(){
+   $("section").mouseenter(function(){
    	 var id = $(this).attr('id');
    	 $('a').removeClass('detect-scroll-area');
    	 $("[href=#"+id+"]").addClass('detect-scroll-area');
