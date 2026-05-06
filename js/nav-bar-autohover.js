f13f8a1579b3b8ef731dd453887c9f320e4ae96d
You <you@been.hack>
2026-05-06 04:47:01 +0200

    new simpler version

--- js/nav-bar-autohover.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-   $("section").mouseenter(function(){
-   	 var id = $(this).attr('id');
-   	 $('a').removeClass('detect-scroll-area');
-   	 $("[href=#"+id+"]").addClass('detect-scroll-area');
-   });
-
-});
